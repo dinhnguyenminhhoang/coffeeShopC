b2c990c9592ef94230d52bb3c81edbe7b2e1a6db
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2024-07-15 15:34:17 +0700

    settup router and not Found page

--- FeCoffeeShop/src/App.jsx
+++ FeCoffeeShop/src/App.jsx
@@ -1,3 +1,28 @@
-export default function App() {
-    return <h1 className="text-3xl font-bold underline ">Hello world!</h1>;
-}
+import React from "react";
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import PageNotFound from "./Pages/PageNotFound/PageNotFound";
+
+const App = () => {
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/">
+                <Route index element={<Home />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Route>
+        )
+    );
+
+    return (
+        <div className="App">
+            <RouterProvider router={router} />
+        </div>
+    );
+};
+
+export default App;
