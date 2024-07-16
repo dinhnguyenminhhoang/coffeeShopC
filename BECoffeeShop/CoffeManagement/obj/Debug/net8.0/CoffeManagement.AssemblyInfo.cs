a56ad16b3bda797cfc546ddb9a2d9c26fbb64dba
maiquocthinh <majquocthjnh@gmail.com>
2024-07-16 23:14:42 +0700

    add: Remote SQLServer ConnectString

--- BECoffeeShop/CoffeManagement/obj/Debug/net8.0/CoffeManagement.AssemblyInfo.cs
+++ BECoffeeShop/CoffeManagement/obj/Debug/net8.0/CoffeManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+243473303f699b85b8150642b616a7ddcae12a6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d4a159f54494b6f36d61769aa36953a05a55554")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
