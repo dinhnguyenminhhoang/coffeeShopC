28466fa9789f537da3d1bc292b5534aecadffffc
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2024-07-16 10:06:36 +0700

    DrinksSize page

---
+++ FeCoffeeShop/src/Components/FormManager/DrinksSizeForm.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Form, Input, Button } from "antd";
+
+const DrinksSizeForm = ({ initialValues, onSave, onCancel }) => {
+    const [form] = Form.useForm();
+
+    const handleFinish = (values) => {
+        onSave(values);
+    };
+
+    return (
+        <Form
+            form={form}
+            initialValues={initialValues}
+            onFinish={handleFinish}
+            layout="vertical"
+        >
+            <Form.Item
+                name="name"
+                label="Name"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input the drinks size name!",
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+            <Form.Item
+                name="size"
+                label="Size"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input the drinks size!",
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+            <Form.Item
+                name="price"
+                label="Price"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input the drinks size price!",
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit">
+                    Save
+                </Button>
+                <Button onClick={onCancel} style={{ marginLeft: "8px" }}>
+                    Cancel
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default DrinksSizeForm;
