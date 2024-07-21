90f59535ac229f9779b3d98050ac66e13d7191dc
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2024-07-21 07:57:50 +0700

    setup api customer

---
+++ FeCoffeeShop/src/service/Customer.js
@@ -0,0 +1,32 @@
+import instance from "../config/instance";
+import Cookies from "js-cookie";
+const token = Cookies.get("AccessToken");
+const headers = {
+    "Content-Type": "application/json",
+    accept: "*/*",
+    Authorization: `Bearer ${token}`,
+};
+const getAllCustomers = ({ listParam }) => {
+    return instance.get(`/Customers`, { headers, params: listParam });
+};
+const getCustomersDetaiil = ({ customerId }) => {
+    return instance.get(`/Customers/${customerId}`, { headers });
+};
+
+const createCustomers = ({ formData }) => {
+    return instance.post(`/Customers`, formData, { headers });
+};
+const updateCustomers = ({ formData }) => {
+    return instance.put(`/Customers`, formData, { headers });
+};
+const deleteCustomers = ({ customerId }) => {
+    return instance.delete(`/Customers/${customerId}`, { headers });
+};
+
+export {
+    getAllCustomers,
+    createCustomers,
+    getCustomersDetaiil,
+    updateCustomers,
+    deleteCustomers,
+};
